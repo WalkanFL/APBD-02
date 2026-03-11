6bb0c91a88094deb6172004f4d41f748a2a59929
WalkanFL <maciej.b.szczepanski@o2.pl>
2026-03-11 09:42:54 +0100

    Added base average function

--- ConsoleApp1/ConsoleApp1/System.cs
+++ ConsoleApp1/ConsoleApp1/System.cs
@@ -3,6 +3,7 @@
 public class System
 {
     private Entry input;
+
     public System(Entry input)
     {
         this.input = input;
@@ -13,4 +14,10 @@ public class System
         input.ExtractPower();
         input.ExtractSpeed();
     }
+
+    public double calculateAverage(int[] values)
+    {
+        return values.Average();
+    }
+
 }
